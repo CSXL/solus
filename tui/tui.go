ff09587bfb682c7c5fd938e3869955ac1708ed98
ecsbeats <adamb@csxlabs.org>
2023-03-08 09:55:02 -0800

    add lipgloss

--- tui/tui.go
+++ tui/tui.go
@@ -3,6 +3,7 @@ package tui
 import (
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type keyMap struct {
@@ -14,6 +15,11 @@ type keyMap struct {
 	Help  key.Binding
 }
 
+type styleMap struct {
+	primary   lipgloss.Style
+	secondary lipgloss.Style
+}
+
 var (
 	keybindings = keyMap{
 		Up:    key.NewBinding(key.WithKeys("up"), key.WithHelp("up", "Move up")),
@@ -22,6 +28,10 @@ var (
 		Right: key.NewBinding(key.WithKeys("right"), key.WithHelp("right", "Move right")),
 		Quit:  key.NewBinding(key.WithKeys("ctrl+c", "esc"), key.WithHelp("ctrl+c", "Quit")),
 		Help:  key.NewBinding(key.WithKeys("h", "?"), key.WithHelp("h", "Show help")),
+	}
+	styles = styleMap{
+		primary:   lipgloss.NewStyle().Foreground(lipgloss.Color("#f8f8f2")),
+		secondary: lipgloss.NewStyle().Foreground(lipgloss.Color("#4E0069")),
 	}
 )
 
@@ -58,7 +68,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return "Hello, World!"
+	var s string
+	s += styles.primary.Render("Hello, World!")
+	s += "\n"
+	s += styles.secondary.Render("Hola, tierra!")
+	s += "\n"
+	return s
 }
 
 func Run() (tea.Model, error) {
