8c4a112e7ad9bb472120a2e39e89d8fdff0676cd
ecsbeats <adamb@csxlabs.org>
2023-03-16 23:51:42 -0700

    cimplement and test OpenAI text completion and OpenAI's Codex

--- api/openai.go
+++ api/openai.go
@@ -1,17 +1,45 @@
 package api
 
 import (
+	"context"
+
 	openai "github.com/sashabaranov/go-openai"
 )
 
 type OpenAI struct {
 	apiKey string
+	ctx    context.Context
 	client *openai.Client
 }
 
 func NewOpenAI(apiKey string) *OpenAI {
 	return &OpenAI{
 		apiKey: apiKey,
+		ctx:    context.Background(),
 		client: openai.NewClient(apiKey),
 	}
+}
+
+func NewOpenAIWithBaseURL(apiKey string, baseURL string) *OpenAI {
+	cfg := openai.DefaultConfig(apiKey)
+	cfg.BaseURL = baseURL
+	return &OpenAI{
+		apiKey: apiKey,
+		ctx:    context.Background(),
+		client: openai.NewClientWithConfig(cfg),
+	}
+}
+
+func (o *OpenAI) GetCompletion(prompt string, model string) (string, error) {
+	resp, err := o.client.CreateCompletion(
+		o.ctx,
+		openai.CompletionRequest{
+			Prompt: prompt,
+			Model:  model,
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Text, nil
 }
