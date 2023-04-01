fc93bb55cfa9976df893da63f678a0842fd63402
ecsbeats <adamb@csxlabs.org>
2023-03-31 17:16:26 -0700

    Revert "(refactor) use method extraction to extract language model bridge"
    This reverts commit a6126b1661854f7932a3768d379a59b4952f975f.

--- api/chat_message.go
+++ api/chat_message.go
@@ -9,6 +9,27 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
+type AIMessage struct {
+	Type    string `json:"type"`
+	Content string `json:"content"`
+}
+
+func (msg *AIMessage) GetContent() string {
+	return msg.Content
+}
+
+func (msg *AIMessage) GetType() string {
+	return msg.Type
+}
+
+func (msg *AIMessage) IsQuery() bool {
+	return msg.Type == "query"
+}
+
+func (msg *AIMessage) IsMessage() bool {
+	return msg.Type == "message"
+}
+
 type ChatMessage struct {
 	Content string
 	Role    string
@@ -22,13 +43,14 @@ func (msg ChatMessage) GetRole() string {
 	return msg.Role
 }
 
-func (msg *ChatMessage) ToAIMessage() (*AIMessage, error) {
+func (msg *ChatMessage) ToAIMessage() (AIMessage, error) {
 	marshalledContent := msg.GetContent()
-	AIMessage, err := NewAIMessageFromJSONString(marshalledContent)
+	var unMarshalledContent AIMessage
+	err := json.Unmarshal([]byte(marshalledContent), &unMarshalledContent)
 	if err != nil {
-		return AIMessage, err
+		return unMarshalledContent, nil
 	}
-	return AIMessage, nil
+	return unMarshalledContent, err
 }
 
 type ChatClient struct {
@@ -89,21 +111,12 @@ func (c *ChatClient) SaveMessages(filename string) error {
 	return nil
 }
 
-func (c *ChatClient) AddMessage(role string, content string) error {
-	aiMessage := NewAIMessage("message", content)
-	marshalledContent, err := aiMessage.ToJSONString()
-	if err != nil {
-		return err
-	}
-	c.messages = append(c.messages, ChatMessage{marshalledContent, role})
-	return nil
+func (c *ChatClient) AddMessage(role string, content string) {
+	c.messages = append(c.messages, ChatMessage{content, role})
 }
 
 func (c *ChatClient) SendMessage(content string, role string) error {
-	err := c.AddMessage(role, content)
-	if err != nil {
-		return err
-	}
+	c.AddMessage(role, content)
 	messages, err := c.CreateChatCompletion(c.messages, openai.GPT3Dot5Turbo)
 	c.messages = messages
 	return err
