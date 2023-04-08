d0f191492b58f889ea00944f656b099634be8da4
ecsbeats <adamb@csxlabs.org>
2023-04-08 02:40:54 -0700

    add openai test JSON chat response

--- ai/openai/test_endpoints.go
+++ ai/openai/test_endpoints.go
@@ -9,7 +9,8 @@ import (
 type OpenAIResponse string
 
 const (
-	SampleChatCompletion OpenAIResponse = `{"id":"chatcmpl-123","object":"chat.completion","created":1679367552,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":11,"total_tokens":20},"choices":[{"message":{"role":"assistant","content":"\n\nHi there! How may I assist you today?"},"finish_reason":"stop","index":0}]}`
+	SampleChatCompletion     OpenAIResponse = `{"id":"chatcmpl-123","object":"chat.completion","created":1679367552,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":11,"total_tokens":20},"choices":[{"message":{"role":"assistant","content":"\n\nHi there! How may I assist you today?"},"finish_reason":"stop","index":0}]}`
+	SampleChatJSONCompletion OpenAIResponse = `{"id":"chatcmpl-123","object":"chat.completion","created":1679367552,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":11,"total_tokens":20},"choices":[{"message":{"role":"assistant","content":"{\"type\":\"message\",\"content\":\"CSX Labs is an amazing organization.\"}"},"finish_reason":"stop","index":0}]}`
 )
 
 func StartHTTPTestServer(response OpenAIResponse) *httptest.Server {
