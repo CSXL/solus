d560ca9a1599a0cb27719870aecfbb47c8c372af
ecsbeats <adamb@csxlabs.org>
2023-04-08 19:59:31 -0700

    implement barebones requirements API

--- ai/openai/test_endpoints.go
+++ ai/openai/test_endpoints.go
@@ -11,6 +11,7 @@ type OpenAIResponse string
 const (
 	SampleChatCompletion     OpenAIResponse = `{"id":"chatcmpl-123","object":"chat.completion","created":1679367552,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":11,"total_tokens":20},"choices":[{"message":{"role":"assistant","content":"\n\nHi there! How may I assist you today?"},"finish_reason":"stop","index":0}]}`
 	SampleChatJSONCompletion OpenAIResponse = `{"id":"chatcmpl-123","object":"chat.completion","created":1679367552,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":11,"total_tokens":20},"choices":[{"message":{"role":"assistant","content":"{\"type\":\"message\",\"content\":\"CSX Labs is an amazing organization.\"}"},"finish_reason":"stop","index":0}]}`
+	SampleChatYAMLCompletion OpenAIResponse = `{"id":"chatcmpl-123","object":"chat.completion","created":1679367552,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":11,"total_tokens":20},"choices":[{"message":{"role":"assistant","content":"mission: To advance technology for humanity\nrequirements:\n  - requirement1\n  - requirement2\n"},"finish_reason":"stop","index":0}]}`
 )
 
 func StartHTTPTestServer(response OpenAIResponse) *httptest.Server {
