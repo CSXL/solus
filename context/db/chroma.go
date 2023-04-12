8fbd321ecfd14dc45e856b797fc49dc98e910191
ecsbeats <adamb@csxlabs.org>
2023-04-11 23:01:21 -0700

    add chroma AddDocuments

--- context/db/chroma.go
+++ context/db/chroma.go
@@ -4,21 +4,31 @@ import (
 	"context"
 
 	chromadb "github.com/CSXL/go-chroma"
+	"github.com/CSXL/solus/ai"
+	"github.com/CSXL/solus/ai/openai"
 )
 
 // ChromaClient is a client for interacting with the Chroma database
 type ChromaClient struct {
-	context *context.Context
-	db      *chromadb.Client
+	context      *context.Context
+	db           *chromadb.Client
+	aiConfig     *ai.AIConfig
+	openAIClient *openai.OpenAI
 }
 
 // NewChromaClient creates a new ChromaClient
-func NewChromaClient(ctx *context.Context, basePath string) (*ChromaClient, error) {
+func NewChromaClient(ctx *context.Context, basePath string, aiConfig ai.AIConfig) (*ChromaClient, error) {
 	chromadbClient, err := chromadb.NewClient(basePath)
+	if err != nil {
+		return nil, err
+	}
+	openAIClient := openai.NewOpenAI(aiConfig.OpenAIAPIKey)
 	return &ChromaClient{
-		context: ctx,
-		db:      chromadbClient,
-	}, err
+		context:      ctx,
+		db:           chromadbClient,
+		aiConfig:     &aiConfig,
+		openAIClient: openAIClient,
+	}, nil
 }
 
 // GetChromaClient returns the internal chromadb client
@@ -31,4 +41,53 @@ func (c *ChromaClient) GetContext() *context.Context {
 	return c.context
 }
 
-// AddDocument adds a document to the database
+// GetEmbeddings returns the embeddings for the given texts
+func (c *ChromaClient) GetEmbeddings(texts []string) ([][]float32, error) {
+	return c.openAIClient.GetEmbeddings(texts)
+}
+
+// AddDocuments adds documents to the database
+func (c *ChromaClient) AddDocuments(collection string, documents []*Document) error {
+	IDs := make([]interface{}, len(documents))
+	Metadatas := make([]interface{}, len(documents))
+	Embeddings := make([]interface{}, len(documents))
+	Contents := make([]interface{}, len(documents))
+	for i, document := range documents {
+		IDs[i] = document.ID
+		Metadatas[i] = document.Metadata
+		Embeddings[i] = document.Embedding
+		Contents[i] = document.Content
+	}
+	chromaDocuments := chromadb.AddEmbedding_Documents{}
+	chromaDocuments.FromAddEmbeddingDocuments1(chromadb.AddEmbeddingDocuments1(Contents))
+	chromaEmbeddings := Embeddings
+	chromaIds := chromadb.AddEmbedding_Ids{}
+	chromaIds.FromAddEmbeddingIds1(IDs)
+	chromaMetadatas := chromadb.AddEmbedding_Metadatas{}
+	chromaMetadatas.FromAddEmbeddingMetadatas0(Metadatas)
+	IncrementIndex := true
+	addRequest := chromadb.AddEmbedding{
+		Documents:      &chromaDocuments,
+		Embeddings:     chromaEmbeddings,
+		Ids:            &chromaIds,
+		IncrementIndex: &IncrementIndex,
+		Metadatas:      &chromaMetadatas,
+	}
+	_, err := c.db.Add(*c.GetContext(), collection, addRequest)
+	return err
+}
+
+type Document struct {
+	ID        string
+	Metadata  []interface{}
+	Embedding []interface{}
+	Content   string
+}
+
+func NewDocument(id string, metadata []interface{}, content string) *Document {
+	return &Document{
+		ID:       id,
+		Metadata: metadata,
+		Content:  content,
+	}
+}
--- context/db/chroma_test.go
+++ context/db/chroma_test.go
@@ -4,12 +4,42 @@ import (
 	"context"
 	"testing"
 
+	"github.com/CSXL/solus/ai"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewChromaClient(t *testing.T) {
 	ctx := context.Background()
-	client, err := NewChromaClient(&ctx, "https://example.com")
+	aiConfig := ai.NewAIConfig("testKey")
+	client, err := NewChromaClient(&ctx, "https://example.com", *aiConfig)
 	assert.Nil(t, err)
 	assert.NotNil(t, client)
+}
+
+func TestGetChromaDB(t *testing.T) {
+	ctx := context.Background()
+	aiConfig := ai.NewAIConfig("testKey")
+	client, err := NewChromaClient(&ctx, "https://example.com", *aiConfig)
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.NotNil(t, client.GetChromaDB())
+}
+
+func TestGetContext(t *testing.T) {
+	ctx := context.Background()
+	aiConfig := ai.NewAIConfig("testKey")
+	client, err := NewChromaClient(&ctx, "https://example.com", *aiConfig)
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.NotNil(t, client.GetContext())
+}
+
+func TestAddDocuments(t *testing.T) {
+	ctx := context.Background()
+	aiConfig := ai.NewAIConfig("testKey")
+	client, err := NewChromaClient(&ctx, "https://example.com", *aiConfig)
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	err = client.AddDocuments("test", []*Document{})
+	assert.Nil(t, err)
 }
